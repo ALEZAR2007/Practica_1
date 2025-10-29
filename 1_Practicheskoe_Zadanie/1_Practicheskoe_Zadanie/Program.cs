648ad78782c3bcea238e2a1e49916a0ab264d390
ALEZAR2007 <167108046+ALEZAR2007@users.noreply.github.com>
2025-10-29 13:38:27 +0900

    Add files via upload

---
+++ 1_Practicheskoe_Zadanie/1_Practicheskoe_Zadanie/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Program_1
+{
+    public static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+        int w = a + b;
+        Console.WriteLine($"Сумма чисел {a} и {b} равна: {w}");
+
+        Console.Write("Введите первое число: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите второе число: ");
+        int d = Convert.ToInt32(Console.ReadLine());
+        int q = c - d;
+        Console.WriteLine($"Разность чисел {c} и {d} равна: {q}");
+    }
+}
+
+
